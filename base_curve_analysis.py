e0f0c14cc876892838694778715223d3f9116ad2
charm-dvrk <chuazh@stanford.edu>
2019-03-26 20:34:15 -0700

    updated base_curve_analysis, kinda... I just think I can interpret the code better now so it eats the CSV files with the micron tracker data.

--- base_curve_analysis.py
+++ base_curve_analysis.py
@@ -13,7 +13,7 @@ def loadfile(filename):
     data = data[1:-1, :]
     return data
 
-
+'''
 def split_data(data, displacement_level):
     # returns an array for that target displacement level
     idx = np.where(data[:, 0] == displacement_level)
@@ -31,6 +31,20 @@ def split_data(data, displacement_level):
     output = [output1, output2]
 
     return output
+'''
+def split_data(data, displacement_level):
+    # returns an array for that target displacement level
+    idx = np.where(data[:, 0] == displacement_level)
+    split_disp = data[idx]
+    max_trials = np.amax(split_disp[:,1])
+    idxes =[]
+    output=[]
+    for i in range(0,int(max_trials)):
+        idxes.append(np.where(split_disp[:, 1] == i+1))
+        output.append(split_disp[idxes[i]])
+        output[i][:,6] = output[i][:,6]-output[i][0,6]
+
+    return output
 
 
 def interpAndAvg(data):
@@ -41,7 +55,7 @@ def interpAndAvg(data):
 
     # split the array into elongation and relaxation portions
     for i in range(0, numData):
-        idx_max = np.argmax(data[i][:, 6])
+        idx_max = np.argmax(data[i][:, 13])
         new_data.append(data[i][0:idx_max, :])
         new_data.append(data[i][idx_max:-1, :])
 
@@ -55,18 +69,18 @@ def interpAndAvg(data):
         scrubbed_data.append(check_unique(new_data[i]))
 
         if i > 0:
-            interped_forces.append(np.interp(scrubbed_data[0][:, 3], scrubbed_data[i][:, 3], scrubbed_data[i][:, 6]))
-            interped_displacements.append(scrubbed_data[i][:, 3])
+            interped_forces.append(np.interp(scrubbed_data[0][:, 6], scrubbed_data[i][:, 6], scrubbed_data[i][:, 13]))
+            interped_displacements.append(scrubbed_data[i][:, 6])
             mean_force_arr = np.vstack((mean_force_arr, np.array(interped_forces[i])))
             # mean_disp_arr = np.vstack((mean_disp_arr, np.array(interped_displacements[i])))
         else:
-            interped_forces.append(scrubbed_data[i][:, 6])
-            interped_displacements.append(scrubbed_data[i][:, 3])
+            interped_forces.append(scrubbed_data[i][:, 13]) # initialize the force data points
+            interped_displacements.append(scrubbed_data[i][:, 6]) # initialize the displacement data points
             mean_force_arr = np.array(interped_forces[i])
             # mean_disp_arr = np.array(interped_displacements[i])
 
     mean_force = np.mean(mean_force_arr, 0)
-    mean_disp = scrubbed_data[0][:, 3]
+    mean_disp = scrubbed_data[0][:, 6]
     # mean_disp = np.mean(mean_disp_arr,0)
 
     return mean_disp, mean_force
@@ -74,28 +88,29 @@ def interpAndAvg(data):
 
 def check_unique(data):
     # takes in an array
-    val, idxF = np.unique(data[:, 6], return_index=True)
+    val, idxF = np.unique(data[:, 13], return_index=True)
     scrubbed_data = data[idxF]
-    val, idxD = np.unique(scrubbed_data[:, 3], return_index=True)
+    val, idxD = np.unique(scrubbed_data[:, 6], return_index=True)
     unique_data = scrubbed_data[idxD]
 
     return unique_data
 
 
-def plot_raw(segmented_data):
+def plot_raw(segmented_data,color):
     list_size = len(segmented_data)
 
-    plt.figure()
+    #plt.figure()
 
     for i in range(0, list_size):
         numTrials = len(segmented_data[i])
 
         for j in range(0, numTrials):
-            force = segmented_data[i][j][:, 6]
-            displacement = -segmented_data[i][j][:, 3]
-            plt.plot(displacement, force)
+            force = segmented_data[i][j][:, 13]
+            displacement = -segmented_data[i][j][:, 6]
+            plt.plot(displacement, force,color=color)
+            #plt.plot(displacement, force)
 
-    plt.show()
+    #plt.show()
 
 
 def plot_avgs(displacement_list, force_list):
@@ -127,7 +142,7 @@ def fit_poly3(displacement_list, force_list):
             aggregate_forces = np.hstack((aggregate_forces, force_list[i]))
             aggregate_disp = np.hstack((aggregate_disp, displacement_list[i]))
 
-    coeffs = np.polyfit(-aggregate_disp, aggregate_forces, 5)
+    coeffs = np.polyfit(-aggregate_disp, aggregate_forces, 3)
     p = np.poly1d(coeffs)
 
     return p
@@ -181,11 +196,14 @@ if __name__ == "__main__":
     filename = "base_curve"
 
     p = []
-
-    for j in range(0, 1):
+    color = pl.cm.jet(np.linspace(0,1,19))
+    
+    for j in range(0, 19):
         #filename = "basecurves_111318/base_curve" + str(j + 1) + "_ds.csv"
+        filename = "basecurves_031419/" + str(j + 1) + ".csv"
+        print(filename)
         #filename = "basecurve2_" + str(j+1) + ".csv"
-        filename = "stress_test/base_curve" + str(j + 1) + "_ds.csv"
+        #filename = "stress_test/base_curve" + str(j + 1) + "_ds.csv"
         data = loadfile(filename)
         target_displacements = np.unique(data[:, 0])
         segmented_data = []
@@ -197,21 +215,26 @@ if __name__ == "__main__":
             average_disp, average_force = interpAndAvg(segmented_data[i])
             average_disp_curves.append(average_disp)
             average_force_curves.append(average_force)
-
-        plot_raw(segmented_data)
+        
+        
+        
+        plot_raw(segmented_data,color[j])
+        
         plot_avgs(average_disp_curves, average_force_curves)
         p.append(fit_poly3(average_disp_curves, average_force_curves))
         plot_poly3(p[j], average_disp_curves, average_force_curves)
         plt.close("all")
+        
+    plt.show()
 
     plt.figure()
-    curves_we_like = [0,3,5]
+    curves_we_like = [7]
     #curves_we_like = [0,1,2]
     f_curves_we_like = []
     d_curves_we_like = []
 
     ref_curve_idx = 0
-    '''
+    
     for i in curves_we_like:
         x_offset = np.amax(np.roots(p[ref_curve_idx])[np.iscomplex(np.roots(p[ref_curve_idx]))==False])-np.amax(np.roots(p[i])[np.iscomplex(np.roots(p[i]))==False])
         f_curves_we_like.append(p[i](-(average_disp_curves[-1]+x_offset)))
@@ -219,7 +242,7 @@ if __name__ == "__main__":
     p_avg = fit_poly3(d_curves_we_like,f_curves_we_like)
     plot_poly3(p_avg,d_curves_we_like,f_curves_we_like)
     plt.show()
-    '''
+    
     plt.figure()
 
     color = pl.cm.jet(np.linspace(0,1,len(p)))
@@ -233,10 +256,12 @@ if __name__ == "__main__":
         #x_offset = np.roots(p[ref_curve_idx])[np.iscomplex(np.roots(p[ref_curve_idx]))==False][1]-np.roots(p[i])[np.iscomplex(np.roots(p[i]))==False][1]
         #plt.plot(-average_disp_curves[-1], p[i](-(average_disp_curves[-1]+x_offset)),color=color[i])
 
-        plt.plot(-average_disp_curves[-1], p[i](-(average_disp_curves[-1]))+y_offset,color=color[i])
+        x = np.linspace(-60,0,100)
+        plt.plot(-x, p[i](-x)+y_offset,color=color[i])
+        #plt.plot(-average_disp_curves[-1], p[i](-(average_disp_curves[-1]))+y_offset,color=color[i])
         #plt.xlim((0,0.045))
-        major_ticks = np.arange(0, 0.06, 0.001)
-        minor_ticks = np.arange(0, 0.06, 0.0001)
+        major_ticks = np.arange(0, 60, 1)#major_ticks = np.arange(0, 0.06, 0.001)
+        minor_ticks = np.arange(0, 60, 0.1)#minor_ticks = np.arange(0, 0.06, 0.0001)
         ax.set_xticks(major_ticks)
         ax.set_xticks(minor_ticks, minor=True)
         # And a corresponding grid
@@ -245,11 +270,11 @@ if __name__ == "__main__":
         ax.grid(which='minor', alpha=0.75)
         ax.grid(which='major', alpha=0.75)
 
-    plt.legend(["1", '2', '3', '4', '5', '6','2 rpt','5 rpt'], loc=0)
+    plt.legend(["1", '2', '3', '4', '5', '6','7','8','9','10','11','12','13','14','15','16','17','18','19'], loc=0)
 
     #plt.plot(-average_disp_curves[-1], p_avg(-(average_disp_curves[-1]+x_offset)),lw=5)
     plt.show()
-    force_bounds = get_force_error_bounds(p_avg,[1,1.5,2.5,4,5.5],0.002)
+    force_bounds = get_force_error_bounds(p_avg,[1.5,2.5,3.5,4.5,6],2)
     np.savetxt('force_bounds.csv',force_bounds,'%.3f',delimiter=',')
     coeffs = p_avg.coeffs
     coeffs = coeffs.real
